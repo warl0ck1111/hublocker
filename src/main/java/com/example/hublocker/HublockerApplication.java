2467fe330d081f870cc2562b2b803e6efb487453
warl0ck <bashirokala@gmail.com>
2021-07-11 13:09:58 +0100

    add script runner and in-memory database for initial set up of the server application

--- src/main/java/com/example/hublocker/HublockerApplication.java
+++ src/main/java/com/example/hublocker/HublockerApplication.java
@@ -1,13 +1,57 @@
 package com.example.hublocker;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
+import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+
+@Slf4j
 @SpringBootApplication
-public class HublockerApplication {
+public class HublockerApplication implements ApplicationListener<ContextRefreshedEvent> {
+
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Value("${databaseAlreadySetup}")
+	boolean alreadySetup;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HublockerApplication.class, args);
 	}
 
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		if(alreadySetup) return;
+		scriptRunner("hub-locker.sql");
+	}
+
+	/**
+	 * @param sqlFile
+	 */
+	public void scriptRunner(String sqlFile) {
+
+
+		try {
+
+			// Initialize object for ScripRunner
+			ScriptRunner sr = new ScriptRunner(dataSource.getConnection());
+			// Give the input file to Reader
+			Reader reader = new BufferedReader(new FileReader(sqlFile));
+			// Execute script
+			sr.runScript(reader);
+		} catch (Exception e) {
+			log.info("Failed to Execute{0} The error is {1}", new Object[]{sqlFile, e.getMessage()});
+		}
+	}
 }
