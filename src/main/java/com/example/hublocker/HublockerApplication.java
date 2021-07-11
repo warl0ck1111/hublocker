abd4987046b53cf6a0b9bfde152c4aebce31ea63
warl0ck <bashirokala@gmail.com>
2021-07-11 12:43:26 +0100

    initial commit

---
+++ src/main/java/com/example/hublocker/HublockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hublocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HublockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HublockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hublocker/HublockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hublocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HublockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
